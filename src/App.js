61243fcbee38f815c1876d55abbf5d2317e3f9d2
LudmilaASouza <ludmilasouz.a@hotmail.com>
2023-08-09 15:28:11 -0300

    style

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ const GlobalStyle = createGlobalStyle `
   }
 
   #root {
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
